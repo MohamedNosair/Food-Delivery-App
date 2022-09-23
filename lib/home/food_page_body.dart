77836aaf4002948fddf122a1d60584f609d4e425
Mohamed Nosair <mohamednosair576@gmail.com>
2022-09-23 20:20:29 +0200

    first commit

---
+++ lib/home/food_page_body.dart
@@ -0,0 +1,309 @@
+import 'package:delivery_app/utils/color.dart';
+import 'package:delivery_app/utils/dimensions.dart';
+import 'package:delivery_app/widgets/big_text.dart';
+import 'package:delivery_app/widgets/icon_and_text_widget.dart';
+import 'package:delivery_app/widgets/small_text.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+  @override
+  _FoodPageBodyState createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var currentPageValue = 0.0;
+
+  var scaleFactor = 0.8;
+
+  double height = Dimensions.pageViewContainer;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  void disPose() {
+    pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //slider section
+        Container(
+          height: Dimensions.pageView,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItem(position);
+            },
+          ),
+        ),
+        //dots
+        new DotsIndicator(
+          dotsCount: 5,
+          position: currentPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(Dimensions.radius5)),
+          ),
+        ),
+        //popular text
+        SizedBox(
+          height: Dimensions.height30,
+        ),
+        Container(
+          margin: EdgeInsets.only(left: Dimensions.width10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+         children: [
+           BigText(
+               text: "Popular",
+           ),
+           SizedBox(
+             width: Dimensions.width10,
+           ),
+           Container(
+             margin: EdgeInsets.only(bottom: 3),
+             child: BigText(text: "." , color: Colors.black26,),
+           ),
+           SizedBox(
+             width: Dimensions.width10,
+           ),
+           Container(
+               margin: EdgeInsets.only(bottom: 2),
+               child: SmallText(text: "Food pairing"),
+           ),
+         ],
+          ),
+        ),
+        // list of food and image
+        Container(
+          height: 700,
+          child: ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: 5,
+            itemBuilder: (context , index ) {
+                return Container(
+                  margin: EdgeInsets.only(
+                    left: Dimensions.width10 , right:Dimensions.width10, bottom: Dimensions.height10,
+                  ),
+                  child: Row(
+                    children: [
+                      //image section
+                      Container(
+                        width: 120,
+                        height:120,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Dimensions.radius30),
+                          color: Colors.white38,
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/images/food5.jpg',
+                            ),
+                            fit: BoxFit.cover
+                          ),
+
+                        ),
+
+                      ),
+                      //text container
+                      Expanded(
+                        child: Container(
+                          height: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(Dimensions.radius30),
+                              bottomRight: Radius.circular(Dimensions.radius30),
+                            ),
+                            color: Colors.white,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                              left: Dimensions.width10,
+                              right: Dimensions.width10,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                BigText(text: "Nutritious fruit meal in Egypt"),
+
+                                SmallText(text: "with chinese characteristics"),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    IconAndTextWidget(
+                                      icon: Icons.circle_sharp,
+                                      text: "Normal",
+                                      iconColor: AppColors.iconColor1,
+                                    ),
+                                    IconAndTextWidget(
+                                      icon: Icons.location_on,
+                                      text: "1.7km",
+                                      iconColor: AppColors.mainColor,
+                                    ),
+                                    IconAndTextWidget(
+                                      icon: Icons.access_time,
+                                      text: "32win",
+                                      iconColor: AppColors.iconColor2,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                );
+              },
+
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    Matrix4 matrix = Matrix4.identity();
+    if (index == currentPageValue.floor()) {
+      var currSale = 1 - (currentPageValue - index) * (1 - scaleFactor);
+      var currTrans = height * (1 - currSale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currSale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == currentPageValue.floor() + 1) {
+      var currSale =
+          scaleFactor + (currentPageValue - index + 1) * (1 - scaleFactor);
+      var currTrans = height * (1 - currSale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currSale, 1);
+      matrix = Matrix4.diagonal3Values(1, currSale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == currentPageValue.floor() - 1) {
+      var currSale = 1 - (currentPageValue - index) * (1 - scaleFactor);
+      var currTrans = height * (1 - currSale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currSale, 1);
+      matrix = Matrix4.diagonal3Values(1, currSale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currSale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currSale, 1)
+        ..setTranslationRaw(0, height * (1 - scaleFactor) / 2, 0);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: Dimensions.pageViewContainer,
+            margin: EdgeInsets.only(left: Dimensions.width10, right: Dimensions.width10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(Dimensions.radius30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/food6.jpg"),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: Dimensions.pageViewTextContainer,
+              margin: EdgeInsets.only(left: Dimensions.width30, right: Dimensions.width30, bottom: Dimensions.height30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius30),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5),
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, 0),
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5, 0),
+                  ),
+                ],
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: Dimensions.height15, left: Dimensions.height15, right: Dimensions.height15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(
+                      height: Dimensions.height5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                            5,
+                            (index) => Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                              size: 15,
+                            ),
+                          ),
+                        ),
+                        SmallText(text: "4.5"),
+                        SmallText(text: "1237"),
+                        SmallText(text: "comments"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: Dimensions.height10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7km",
+                          iconColor: AppColors.mainColor,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.access_time,
+                          text: "32win",
+                          iconColor: AppColors.iconColor2,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,35 @@
+import 'package:get/get.dart';
+
+class Dimensions{
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight/2.964 ;
+  static double pageViewContainer = screenHeight/4.5 ;
+  static double pageViewTextContainer = screenHeight/7 ;
+  //height
+  static double height5 = screenHeight/148.26 ;
+  static double height10 = screenHeight/74.13;
+  static double height15 = screenHeight/49.422;
+  static double height30 = screenHeight/24.71;
+  static double height45 = screenHeight/16.47;
+  static double height120 = screenHeight/6.17;
+  //width
+  static double width5 = screenHeight/148.26 ;
+  static double width10 = screenHeight/74.13;
+  static double width15 = screenHeight/49.422;
+  static double width20 = screenHeight/37.065;
+  static double width30 = screenHeight/24.71;
+  static double width45 = screenHeight/16.47;
+  static double width120 = screenHeight/6.17;
+  //font
+  static double font20 = screenHeight/37.065;
+  //radius
+  static double radius5 = screenHeight/148.26;
+  static double radius15 = screenHeight/49.422;
+  static double radius30 = screenHeight/24.71;
+  //icon
+  static double iconSize25 = screenHeight/29.65;
+
+
+}
